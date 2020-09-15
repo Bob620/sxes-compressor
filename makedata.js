de03ecf37f708f6bf61a77d54f816bbf99ad8dcc
bob620 <bruder.kraft225@gmail.com>
2020-09-15 16:56:25 -0500

    fix(qlwdata) length and binning

--- makedata.js
+++ makedata.js
@@ -3,7 +3,7 @@ const constants = require('./constants.json');
 const Data = require('./structures/data.js');
 
 module.exports = (archive, uri, condition) => {
-	switch(uri.split('/').pop().split('.')[0]) {
+	switch (uri.split('/').pop().split('.')[0]) {
 		default:
 			return new Data(archive, uri, 0, condition.ccd.bins.x, condition.ccd.bins.y);
 		case 'xes':
@@ -14,18 +14,27 @@ module.exports = (archive, uri, condition) => {
 };
 
 class XesData extends Data {
-	constructor(archive, uri, condition, rawData=Buffer.from([])) {
+	constructor(archive, uri, condition, rawData = Buffer.from([])) {
 		super(archive, uri, rawData, 0, condition.ccd.bins.x, condition.ccd.bins.y);
 	}
 }
 
 class QlwData extends Data {
-	constructor(archive, uri, rawData=Buffer.from([])) {
-		super(archive, uri, rawData, 0, 1, 4098);
+	constructor(archive, uri, rawData = Buffer.from([])) {
+		super(archive, uri, rawData, 0, 1, 4094);
 	}
 
-	async get(bin=0, position=0) {
-		if (bin < this.bins && position <= this.positions)
+	async get(bin = 0, position = undefined) {
+
+		if (position === undefined && bin < this.bins) {
+			if (this.data.rawData.length === 0)
+				this.data.rawData = await this.data.archive.extract(this.data.uri);
+
+			let data = [];
+			for (let i = 0; i <= this.positions; i++)
+				data.push(this.rawData.readDoubleLE(this.data.offset + (8 * (bin * this.positions + i))));
+			return data;
+		} else if (bin < this.bins && position <= this.positions)
 			if (this.data.rawData.length !== 0)
 				return this.rawData.readDoubleLE(this.data.offset + (8 * (bin * this.positions + position)));
 			else {
